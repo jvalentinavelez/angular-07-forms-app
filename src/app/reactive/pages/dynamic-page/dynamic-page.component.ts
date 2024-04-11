73e2a5558f9a099ec300e8f2a79515b7d6f92ced
Juanita Velez <jvalentina.velez@gmail.com>
2024-04-11 06:13:19 -0500

    Insertar elementos al FormArray

--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
-import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  Form,
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   templateUrl: './dynamic-page.component.html',
@@ -13,6 +20,8 @@ export class DynamicPageComponent {
       ['Death Stranding', Validators.required],
     ]),
   });
+
+  public newFavorite: FormControl = new FormControl('', [Validators.required]);
 
   constructor(private fb: FormBuilder) {}
 
@@ -46,6 +55,14 @@ export class DynamicPageComponent {
     return null;
   }
 
+  onAddToFavorites(): void {
+    if (this.newFavorite.invalid) return;
+    const newGame = this.newFavorite.value;
+    // this.favoriteGames.push(new FormControl(newGame, Validators.required));
+    this.favoriteGames.push(this.fb.control(newGame, Validators.required));
+    this.newFavorite.reset();
+  }
+
   onDeleteFavorite(index: number): void {
     this.favoriteGames.removeAt(index);
   }
@@ -56,6 +73,7 @@ export class DynamicPageComponent {
       return;
     }
     console.log(this.myForm.value);
+    (this.myForm.controls['favoriteGames'] as FormArray) = this.fb.array([]);
     this.myForm.reset();
   }
 }
